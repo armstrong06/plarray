bb508459b9250c4488c9a5a7f65741bdcf0cf2f2
Alysha Armstrong <aa.armstrong6@gmail.com>
2023-02-05 00:20:16 -0700

    Fix station centering, add reference station color

--- SeisArray.js
+++ SeisArray.js
@@ -6,6 +6,7 @@ class SeisArray{
         this.height = 500
         this.width = 500
         this.n_stations = 11
+        this.reference_station = 5
     }
 
     renderArray(){
@@ -21,11 +22,14 @@ class SeisArray{
           .attr("cx", d => d.x)
           .attr("cy", d => d.y)
           .attr("r", 10)
+          .attr("id", d=>'stat'.concat(d.index))
+
+        svg.select("#stat".concat(this.reference_station)).attr("fill", "red")
     }
 
     initializeStations(){
         var stations = d3.range(this.n_stations).map(i =>({
-            x: 10+(this.width/this.n_stations)*i,
+            x: (this.width/this.n_stations/2)+(this.width/this.n_stations)*i,
             y: this.height/2,
             index: i
         }))
